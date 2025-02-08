daeed1a17de917de11b95b1fd0b61c23479024f6
Laysan-10 <marichkahh@gmail.com>
2025-02-08 22:42:05 +0300

    skripts
    создание объекта, "занятость", возвращение к исходной позиции и ее изменение

--- Assets/liaisan/can_move.cs
+++ Assets/liaisan/can_move.cs
@@ -1,24 +1,29 @@
 
 using System.Globalization;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class can_move : MonoBehaviour
 {
 	bool drag = true;
-	Vector3 originalPosition;
-	Vector3 pointScreen;
+	 public Vector3 originalPosition;
+	 Vector3 pointScreen;
 	Vector3 offset;
 	public bool areaReached = false;
 	bool take_meal = false;
-	public varka varka;
+	public varka[] varka;
+	static LayerMask targetlayer;
 	
 	Camera _camera;
-	public GameObject cub;
+	// public GameObject cub;
 	void Start()
 	{
+	targetlayer = LayerMask.GetMask("varka");
 		_camera = FindObjectOfType<Camera>();
-		varka = FindObjectOfType<varka>();
+		varka = FindObjectsOfType<varka>();
+		originalPosition = transform.position;
 	}
 	public  void OnMouseDown()
 	{
@@ -39,26 +44,35 @@ public class can_move : MonoBehaviour
 	void OnMouseUp()
 	{
 	   
-		if (drag && varka!=null )
+		if (drag)
 		{
-			varka.CheckTriggerStay();
-			
-			// if (!areaReached) {  gameObject.transform.position = originalPosition;}//����������� �� ��������� ������� ���� �� � ����������.
-			// else if(areaReached && take_meal) //���� � ���������� � ����� �������� ����.
-			// {
-			// 	// TeleportMeal(_place);
-			// }
-			// else if(!take_meal)
-			// {
-			// 	gameObject.transform.position = originalPosition;
-			// }
-			// 	drag = false;
+			Debug.Log("Drag");
+			Collider[] varka = Physics.OverlapSphere(transform.position,transform.localScale.x, targetlayer);
+			foreach(var col in varka)
+			{
+				
+				if(col.GetComponent<varka>()==true){
+				// Debug.Log("return" + col.GetComponent<varka>().CheckTriggerStay());
+					if(col.GetComponent<varka>().CheckTriggerStay()==false)
+					{
+						gameObject.transform.position = originalPosition;//занята, значит возвращаем
+
+					}
+					
+					return;
+				}
+			}
+					
+					gameObject.transform.position = originalPosition;
+
+				
+
 		}
-	}
-	void  Update()
-	{
-		// varka.CheckTriggerStay();
+		
+			
+		
 	}
 	
+
 	
 }
--- Assets/liaisan/varka.cs
+++ Assets/liaisan/varka.cs
@@ -5,16 +5,32 @@ public class varka : MonoBehaviour
 {
 	[SerializeField] LayerMask targetlayer;
 	[SerializeField] create_meal create_Meal;
-   public void CheckTriggerStay()
+	bool _is_use = false;
+   public bool CheckTriggerStay()
 	{
-		Collider[] colliders = Physics.OverlapSphere(transform.position,transform.localScale.x, targetlayer); // Определяем коллайдеры в радиусе 0.5
+			Debug.Log("Start"+_is_use);
+		
+		if(_is_use == false)
+		{
+			Collider[] colliders = Physics.OverlapSphere(transform.position,transform.localScale.x, targetlayer); // Определяем коллайдеры в радиусе 0.5
+		// Debug.Log("About Collider" + (colliders == null));
+			
 		foreach (Collider col in colliders)
 		{
 			
-				Debug.Log("Объект с тегом '"  + col.gameObject.name);
-				col.gameObject.layer =default;//что бы объекты спавнились только из-за объекта который опр маску
+				// Debug.Log("Объект с тегом '"  + col.gameObject.name);
+				// Debug.Log("About Collider" + colliders == null);
+				col.gameObject.layer = default;//что бы объекты спавнились только из-за объекта который опр маску
+				col.gameObject.transform.position = gameObject.transform.position;
+				col.GetComponent<can_move>().originalPosition = gameObject.transform.position;
 				create_Meal.Spawn();
-			
+				_is_use = true;
+				return true;
 		}
+		
+		}
+			Debug.Log("ENd" + _is_use);
+		
+		return false;
 	}
 }
