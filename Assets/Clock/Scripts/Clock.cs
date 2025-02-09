ea5c82d3df5802d04ed39b732c4de65f6f708871
Laysan-10 <marichkahh@gmail.com>
2025-02-09 20:50:38 +0300

    water and clock

---
+++ Assets/Clock/Scripts/Clock.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	//-- set start time 00:00
+   
+	public int seconds = 0;
+	public bool realTime=true;
+	
+	public GameObject pointerSeconds;
+	// public GameObject pointerMinutes;
+	// public GameObject pointerHours;
+	
+	//-- time speed factor
+	public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
+
+	//-- internal vars
+	float msecs=0;
+
+void Start() 
+{
+	//-- set real time
+	// if (realTime)
+	// {
+	// 	hour=System.DateTime.Now.Hour;
+	// 	minutes=System.DateTime.Now.Minute;
+	// 	seconds=System.DateTime.Now.Second;
+	// }
+}
+
+void Update() 
+{
+	//-- calculate time
+	msecs += Time.deltaTime * clockSpeed;
+	if(msecs >= 1.0f)
+	{
+		msecs -= 1.0f;
+		seconds++;
+		if(seconds >= 60)
+		{
+			seconds = 0;
+			
+		}
+	}
+
+
+	//-- calculate pointer angles
+	float rotationSeconds = (360.0f / 60.0f)  * seconds;
+	
+
+	//-- draw pointers
+	pointerSeconds.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationSeconds);
+	// pointerMinutes.transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationMinutes);
+	// pointerHours.transform.localEulerAngles   = new Vector3(0.0f, 0.0f, rotationHours);
+	
+	
+}
+}
--- Assets/liaisan/varka.cs
+++ Assets/liaisan/varka.cs
@@ -1,11 +1,19 @@
 
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class varka : MonoBehaviour
 {
 	[SerializeField] LayerMask targetlayer;
 	[SerializeField] create_meal create_Meal;
+	MeshRenderer _water;
 	bool _is_use = false;
+	void Awake()
+	{
+		var mesh = gameObject.GetComponentsInChildren<MeshRenderer>();
+		_water = mesh[1];
+		_water.enabled = false;
+	}
    public bool CheckTriggerStay()
 	{
 			Debug.Log("Start"+_is_use);
@@ -25,6 +33,12 @@ public class varka : MonoBehaviour
 				col.GetComponent<can_move>().originalPosition = gameObject.transform.position;
 				create_Meal.Spawn();
 				_is_use = true;
+				_water.enabled = true;
+				// foreach(var child in mesh)
+				// {
+				// 	child.GetComponent<MeshRenderer>().enabled = true;
+				// 	Debug.Log(child.name);
+				// }	
 				return true;
 		}
 		
